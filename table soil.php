2a6b0717a260d0312381518049e313d59036a931
kavyalenin <kavyalenin1997@gmail.com>
2019-03-21 10:22:09 +0530

    Add files via upload

---
+++ table soil.php
@@ -0,0 +1,121 @@
+
+Raw
+Blame
+History
+   
+
+<?php
+
+//Create Data base if not exists
+
+	$servername = "localhost";
+
+	$username = "root";
+
+	$password = "";
+
+
+
+	// Create connection
+
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+
+	if ($conn->connect_error) {
+
+	    die("Connection failed: " . $conn->connect_error);
+
+	}
+
+
+
+	// Create database
+
+	$sql = "CREATE DATABASE espdemo";
+
+	if ($conn->query($sql) === TRUE) {
+
+	    echo "Database created successfully";
+
+	} else {
+
+	    echo "Error creating database: " . $conn->error;
+
+	}
+
+
+
+	$conn->close();
+
+
+
+	echo "<br>";
+
+//Connect to database and create table
+
+	$servername = "localhost";
+
+	$username = "root";
+
+	$password = "";
+
+	$dbname = "espdemo";
+
+
+
+	// Create connection
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+
+	if ($conn->connect_error) {
+
+	    die("Connection failed: " . $conn->connect_error);
+
+	}
+
+
+
+	//Sr No, Station, Status(OK, NM, WM, ACK) Date, Time
+
+	//1         A          NM                 12-5-18    12:15:00 am
+
+	// sql to create table
+
+	$sql = "CREATE TABLE soil (
+
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+	station VARCHAR(30),
+
+	status VARCHAR(30),
+
+	remark VARCHAR(50),
+
+	`Date` DATE NULL,
+
+	`Time` TIME NULL, 
+
+	`TimeStamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+	)";
+
+
+
+	if ($conn->query($sql) === TRUE) {
+
+	    echo "Table soil created successfully";
+
+	} else {
+
+	    echo "Error creating table: " . $conn->error;
+
+	}
+
+
+
+	$conn->close();
+
+?>
